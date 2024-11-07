7eb0cc3cf6115b62c6702c6aca6cd4ccfe88caef
natidaschoolboy <natidaschoolboy@gmail.com>
2024-11-07 21:51:39 +0200

    Log In/Sign UI Completed

--- PRG282_Students_Records_Management_Application/Form1.cs
+++ PRG282_Students_Records_Management_Application/Form1.cs
@@ -24,7 +24,7 @@ namespace PRG282_Students_Records_Management_Application
             if (textBox1.Text == "Username")
             {
                 textBox1.Text = "";
-                textBox1.ForeColor = Color.Black; // Set to regular text color
+                textBox1.ForeColor = Color.Black; 
             }
         }
 
@@ -42,7 +42,7 @@ namespace PRG282_Students_Records_Management_Application
             if (textBox2.Text == "Password")
             {
                 textBox2.Text = "";
-                textBox2.ForeColor = Color.Black; // Set to regular text color
+                textBox2.ForeColor = Color.Black; 
             }
         }
 
@@ -52,6 +52,24 @@ namespace PRG282_Students_Records_Management_Application
             {
                 textBox2.Text = "Password";
                 textBox2.ForeColor = Color.Gray;
+            }
+        }
+
+        private void RemovePlaceholder2(object sender, EventArgs e)
+        {
+            if (textBox3.Text == "Confirm Password")
+            {
+                textBox3.Text = "";
+                textBox3.ForeColor = Color.Black; 
+            }
+        }
+
+        private void SetPlaceholder2(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox3.Text))
+            {
+                textBox3.Text = "Confirm Password";
+                textBox3.ForeColor = Color.Gray;
             }
         }
 
@@ -84,21 +102,60 @@ namespace PRG282_Students_Records_Management_Application
             Panel bottomBorder = new Panel();
            
            
-            // Add Panel to the form
+           
             this.Controls.Add(bottomBorder);
-            textBox1.Text = "Username"; // Set initial placeholder text
+            textBox1.Text = "Username"; 
             textBox1.ForeColor = Color.Gray;
             textBox1.Enter += RemovePlaceholder;
             textBox1.Leave += SetPlaceholder;
 
-            textBox2.Text = "Password"; // Set initial placeholder text
+            textBox2.Text = "Password"; 
             textBox2.ForeColor = Color.Gray;
             textBox2.Enter += RemovePlaceholder1;
             textBox2.Leave += SetPlaceholder1;
 
+            textBox3.Text = "Confirm Password"; 
+            textBox3.ForeColor = Color.Gray;
+            textBox3.Enter += RemovePlaceholder2;
+            textBox3.Leave += SetPlaceholder2;
+
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.FlatAppearance.BorderSize = 0;
+
+            button1.BackColor = Color.Black;
+            button1.Text = "Sign In";
+            button1.ForeColor = Color.White;
+
+
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.FlatAppearance.BorderSize = 0;
+
+            button2.BackColor = Color.Black;
+            button2.Text = "Log In";
+            button2.ForeColor = Color.White;
+            button2.Hide();
+            label1.Text = "Have An Account? Click here to Log In";
+
+            label2.Text = "Sign In";
+            label2.Font = new Font(label2.Font.FontFamily, 40);
 
 
         }
 
+        private void label1_Click(object sender, EventArgs e)
+        {
+            textBox3.Hide();
+            button1.Hide();
+            label1.Hide();
+            button2.Show();
+            label2.Text = "Log In";
+            label2.Font = new Font(label2.Font.FontFamily, 40);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This is where you put the method to sign in");
+        }
     }
 }
